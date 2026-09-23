4a278be549644751c2c27cffec33f1f1187240f7
Ahmed_Taha <154711462+ahmedtahv1@users.noreply.github.com>
2026-09-24 02:34:19 +0300

    Q1

--- ADVC#03/Program.cs
+++ ADVC#03/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //List<int> grades = new()
+            //{
+            //    85,92,78,95,88,70,100,65
+            //};
+            //Console.WriteLine("\n======== Print all ========\n");
+            //Console.WriteLine(string.Join(",",grades));
+            //Console.WriteLine("\n======== Count ========\n");
+            //Console.WriteLine(grades.Count());
+            //Console.WriteLine("\n======== Sprted list ========\n");
+            //grades.Sort();
+            //Console.WriteLine(string.Join(",",grades));
+            //Console.WriteLine("\n======== Above 90 ========\n");
+            //int above90 = grades.Find(x => x > 90);
+            //Console.WriteLine(above90);
+            //Console.WriteLine("\n======== Failing grades ========\n");
+            //List<int> FailingGrades = grades.FindAll(x => x < 75);
+            //Console.WriteLine(string.Join(",",FailingGrades));
+            //Console.WriteLine("\n======== After removed all below 75 ========\n");
+            //grades.RemoveAll(x => x < 75);
+            //Console.WriteLine(string.Join(",", grades));
+            //Console.WriteLine("\n======== Checking ========\n");
+            //Console.WriteLine(grades.Contains(100) ? $"Found it in index : {grades.IndexOf(100)}" : "not found");
+            //Console.WriteLine("\n======== Convert to atring ========\n");
+            //List<string> stringList = new();
+            //foreach(var i in grades)
+            //{
+            //    stringList.Add($"Grade: {i}");
+            //}
+            //Console.WriteLine(string.Join("\n" , stringList)); 
+            #endregion
+
+
+
+
         }
     }
 }
